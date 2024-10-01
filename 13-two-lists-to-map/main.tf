e2c1e409e1ef08bb7b24a0119921b21110a687c4
Raghu K <raghuk.vit@gmail.com>
2024-10-01 06:41:26 +0530

    RoboShop Kubernetes

---
+++ 13-two-lists-to-map/main.tf
@@ -0,0 +1,24 @@
+variable "repos" {
+  default = [
+    "roboshop-cart",
+    "roboshop-catalogue",
+    "roboshop-user",
+    "roboshop-shipping",
+    "roboshop-payment",
+    "roboshop-frontend"
+  ]
+}
+
+variable "ENV" {
+  default = [
+    "DEV",
+    "QA",
+    "UAT",
+    "PROD"
+  ]
+}
+
+output "map" {
+  value = {for k in var.repos: k => k}
+}
+
