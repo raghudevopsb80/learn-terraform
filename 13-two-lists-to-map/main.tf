448801c8366310fc1b40cf90ff2b93a54a0c862d
Raghu K <raghuk.vit@gmail.com>
2024-10-01 06:53:58 +0530

    RoboShop Kubernetes

--- 13-two-lists-to-map/main.tf
+++ 13-two-lists-to-map/main.tf
@@ -18,7 +18,20 @@ variable "ENV" {
   ]
 }
 
+locals {
+  repo_env_map = {
+    for repo in var.repos :
+      for env in var.ENV :
+        "${repo}_${env}" => {
+            repo_name = repo
+            env       = env
+        }
+      }
+}
+
+
 output "map" {
-   value = { for k in var.repos: for i in var.ENV: "${k}_${i}" => {"repo_name" = k, "env" = i}}
+   #value = { for k in var.repos: for i in var.ENV: "${k}_${i}" => {"repo_name" = k, "env" = i}}
+        value = local.repo_env_map
 }
 
