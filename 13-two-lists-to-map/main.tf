48767ea79e10e141d5b0f4272149b17aa03009d3
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-10 06:26:14 +0530

    Roboshop Code

--- 13-two-lists-to-map/main.tf
+++ 13-two-lists-to-map/main.tf
@@ -24,24 +24,24 @@
 
 variable "repos" {
   default = {
-    "roboshop-cart" = {}
+    "roboshop-cart"      = {}
     "roboshop-catalogue" = {}
   }
 }
 
 variable "env" {
   default = {
-    "DEV" = {}
-    "QA" = {}
-    "UAT" = {}
+    "DEV"  = {}
+    "QA"   = {}
+    "UAT"  = {}
     "PROD" = {}
   }
 }
 
 locals {
-  repos_with_envs = {for i,j in var.repos : i => { for x,y in var.env : "${i}_${x}" => {"env" = x, "app" = i} }}
+  repos_with_envs = { for i, j in var.repos : i => { for x, y in var.env : "${i}_${x}" => { "env" = x, "app" = i } } }
 }
 
 output "x" {
-  value = flatten([for a,b in local.repos_with_envs: values(b)])
+  value = flatten([for a, b in local.repos_with_envs : values(b)])
 }
